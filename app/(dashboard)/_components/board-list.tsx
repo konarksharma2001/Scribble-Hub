8a13f5a99bb88eff8a27c78c31fce6b675086fb5
Konark Sharma <sharmakonark6k1@gmail.com>
2024-02-08 14:21:28 +0530

    feat: backend convex routes added along with boardcard component

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -1,8 +1,14 @@
 "use client";
 
+import { useQuery } from "convex/react"; 
+
+import { api } from "@/convex/_generated/api"; 
+
 import { EmptyBoards } from "./empty-boards";
 import { EmptyFavourites } from "./empty-favourites";
 import { EmptySearch } from "./empty-search";
+import { BoardCard } from "./board-card";
+import { NewBoardButton } from "./new-board-button";
 
 interface BoardListProps {
   orgId: string;
@@ -13,7 +19,15 @@ interface BoardListProps {
 }
 
 export const BoardList = ({ orgId, query }: BoardListProps) => {
-  const data = []; // Todo: Change to API call
+  const data = useQuery(api.boards.get, { orgId });
+
+  if(data === undefined) {
+    return (
+      <div>
+        Loading...
+      </div>
+    )
+  }
 
   if (!data?.length && query.search) {
     return <EmptySearch />;
@@ -27,5 +41,28 @@ export const BoardList = ({ orgId, query }: BoardListProps) => {
     return <EmptyBoards />;
   }
 
-  return <div>{JSON.stringify(query)}</div>;
+  return (
+    <div>
+      <h2 className="text-3xl">
+       {query.favourites ? "Favourite boards" : "Team Boards "}
+      </h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4
+      lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+        <NewBoardButton orgId={orgId} />
+        {data?.map((board) => (
+          <BoardCard
+            key={board._id}
+            id={board._id}
+            title={board.title}
+            imageUrl={board.imageUrl} 
+            authorId={board.authorId}
+            authorName={board.authorName}
+            createdAt={board._creationTime}
+            orgId={board.orgId}
+            isFavourite={false}
+          />
+        ))}
+      </div>
+    </div>
+  )
 };
--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -1,7 +1,31 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
 import Image from "next/image";
+import { useMutation } from "convex/react"; 
+
+import { api } from "@/convex/_generated/api";
+import { Button } from "@/components/ui/button";
+import { useOrganization } from "@clerk/nextjs";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { toast } from "sonner";
 
 export const EmptyBoards = () => {
+    const { organization } = useOrganization();
+    const { mutate, pending} = useApiMutation(api.board.create);
+
+    const onClick = () => {
+        if(!organization) return;
+        mutate({
+            orgId: organization.id,
+            title: "Untitled",
+        })
+            .then((id) => {
+                toast.success("Board Created");
+                // TODO: Redirect to board/{id}
+            })
+            .catch(() => toast.error("Failed to create board"));
+    };
+
     return (
         <div className="h-full flex flex-col items-center justify-center">
             <Image
@@ -17,7 +41,7 @@ export const EmptyBoards = () => {
                 Start by creating a board for your organization
             </p>
             <div className="mt-6">
-              <Button size="lg">
+              <Button disabled={pending} onClick={onClick} size="lg">
                  Create Board
               </Button>    
             </div>
