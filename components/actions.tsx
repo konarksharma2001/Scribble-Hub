249c36e4ae0491da2139b7d03dfa4992ef606b7c
Konark Sharma <sharmakonark6k1@gmail.com>
2024-02-10 16:38:45 +0530

    feat: Confirm Modal Added along with delete, rename functionalities in convex

---
+++ components/actions.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { toast } from "sonner";
+import { Link2, Trash2 } from "lucide-react";
+import { DropdownMenuContentProps } from "@radix-ui/react-dropdown-menu";
+
+import { api } from "@/convex/_generated/api";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+
+import { ConfirmModal } from "./confirm-modal";
+import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+
+interface ActionProps {
+  children: React.ReactNode;
+  side?: DropdownMenuContentProps["side"];
+  sideOffset?: DropdownMenuContentProps["sideOffset"];
+  id: string;
+  title: string;
+}
+
+export const Actions = ({
+  children,
+  side,
+  sideOffset,
+  id,
+  title,
+}: ActionProps) => {
+  const { mutate, pending } = useApiMutation(api.board.remove);
+
+  const onCopyLink = () => {
+    navigator.clipboard
+      .writeText(`${window.location.origin}/board/${id}`)
+      .then(() => toast.success("Link Copied"))
+      .catch(() => toast.error("Failed to Copy Link"));
+  };
+
+  const onDelete = () => {
+    mutate({ id })
+      .then(() => toast.success("Board Deleted"))
+      .catch(() => toast.error("Failed to delete board"));
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
+      <DropdownMenuContent
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+        side={side}
+        sideOffset={sideOffset}
+        className="w-60"
+      >
+        <DropdownMenuItem onClick={onCopyLink} className="p-3 cursor-pointer">
+          <Link2 className="h-4 w-4 mr-2" />
+          Copy Board Link
+        </DropdownMenuItem>
+        <ConfirmModal
+          header="Delte Board?"
+          description="This will delete the board and all of its contents."
+          disabled={pending}
+          onConfirm={onDelete}
+        >
+          <Button
+            variant="ghost" 
+            className="p-3 cursor-pointer text-sm w-full justify-start font-normal "
+          >
+            <Trash2 className="h-4 w-4 mr-2" />
+            Delete
+          </Button>
+        </ConfirmModal>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
