df2f5c8b9dd7a82f23e2a56bab288673aae8cd75
Konark Sharma <sharmakonark6k1@gmail.com>
2024-02-12 19:40:14 +0530

    Feat: LiveBlocks Setup Done and canvas screen added

---
+++ app/board/[boardId]/page.tsx
@@ -0,0 +1,23 @@
+import { Canvas } from "./_components/canvas";
+import { Room } from "@/components/room"; 
+import { Loading } from "./_components/loading";
+
+interface BoardIdPageProps {
+    params: {
+        boardId: string;
+    };
+};
+
+const BoardIdPage = ({
+    params
+} : BoardIdPageProps) => {
+    return ( 
+        <div className="h-full w-full">
+            <Room roomId={params.boardId} fallback={<Loading />}>
+                <Canvas boardId={params.boardId} />
+            </Room>
+        </div>
+     );
+};
+ 
+export default BoardIdPage;
